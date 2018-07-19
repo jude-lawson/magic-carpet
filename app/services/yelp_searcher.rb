5603a400fd5023e3177cad76279f4bb9858f80b0
zdcthomas <zdcthomas@yahoo.com>
2018-07-18 21:05:59 -0600

    WIP Faraday call

--- app/services/yelp_searcher.rb
+++ app/services/yelp_searcher.rb
@@ -7,4 +7,15 @@ class YelpSearcher
     # this will connect to a conn method which will make the faraday call
     %w(restaurant1 restaurant2)
   end
+
+  def go_get(info)
+    JSON.parse(conn.get("").body)
+  end
+
+  def conn
+    Faraday.new(:url => "https://api.yelp.com/v3") do |faraday|
+      faraday.headers["Authorization"] = ("Bearer " + token)
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
