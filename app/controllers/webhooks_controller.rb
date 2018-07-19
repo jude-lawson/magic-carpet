339eab96f51c38197f9e373377893b33de8468b5
hmesander <haleymesander@gmail.com>
2018-07-19 15:00:18 -0600

    Updates Webhook Controller to NOT save user ride information in the database

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,11 +1,8 @@
 class WebhooksController < ApplicationController
 
   def receive
-    ride_info = JSON.parse(request.body.read)
     user = User.find_by(lyft_id: ride_info['body']['event']['passenger']['user_id'])
-    ride_id = ride_info['body']['event']['ride_id']
-    ride_status = ride_info['body']['event']['status']
-    user.rides.find(ride_id).update(status: ride_status)
+    ride_info = JSON.parse(request.body.read)
   end
 
   private
