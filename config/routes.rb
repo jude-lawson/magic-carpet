aed4b78fdc4a66ec0a245fd238bc2eeb28bc1ae3
hmesander <haleymesander@gmail.com>
2018-07-19 15:02:42 -0600

    Corrects merge conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   post '/api/v1/adventures', to: 'adventures#create'
-<<<<<<< HEAD
-=======
   get '/auth/lyft', as: :lyft_login
   get '/auth/lyft/callback', to: 'users#create'
   resources :users, only: [:edit]
->>>>>>> 6498d1c334d8b5566be3944047746059f5bb8282
 end
