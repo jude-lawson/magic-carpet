b2178a7461387ec7d2ad476ee54ab2e1008da099
zdcthomas <zdcthomas@yahoo.com>
2018-10-03 10:41:37 -0600

    tests filter exception on impossible criteria

--- spec/services/destinations/destination_filter_spec.rb
+++ spec/services/destinations/destination_filter_spec.rb
@@ -38,5 +38,39 @@ describe 'Filter' do
         expect(result.categories).to_not include(parameters[:restrictions][:categories].first)
       end
     end
+
+    it 'should return an error if the filtered parameters are impossible' do
+      parameters ={
+        search_settings: {
+          'open_now' => true,
+          'radius' => 3000,
+          'latitude' => 39.7293,
+          'longitude' => -104.9844,
+          'price' => "1,2,3",
+          'term' => 'restaurants'
+        },
+        restrictions: {
+            categories: [
+              "italian",
+              "indian"],
+            min_radius: 3000
+          }
+        }
+
+      restaurants = File.open("./fixtures/restaurants.json")
+
+      stub_request(:get, "https://api.yelp.com/v3/businesses/search?latitude=39.7293&longitude=-104.9844&open_now=true&price=1,2,3&radius=3000&term=restaurants").
+      with(
+        headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'Authorization'=>"Bearer #{ENV['yelp_api_key']}",
+       'User-Agent'=>'Faraday v0.12.2'
+        }).
+         to_return(status: 200, body: restaurants, headers: {})
+      dh = DestinationHandler.new(parameters)
+      rests = dh.get_restaurants
+      expect(Filter.remove(parameters[:restrictions], rests) ).to raise_exception
+    end
   end
 end
